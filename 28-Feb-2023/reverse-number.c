9640ccfa724d2d451cb60806898b71faffd3a126
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-02-28 10:25:15 +0600

    selling price calculation

--- 28-Feb-2023/reverse-number.c
+++ 28-Feb-2023/reverse-number.c
@@ -2,9 +2,9 @@
 
 int  main(){
 
-int number, a, b, c, d, reverse;
+int number, a, b, c, d, reverse, first_digit_last_digit_sum, total_sum;
 
-printf("Enter the five digits number:");
+printf("Enter the four digits number:");
 scanf("%d", &number);
 
 a = number % 10;
@@ -20,7 +20,14 @@ d = number % 10;
 
 
 reverse = (a*1000) + (b*100) + (c*10) + d;
-printf("%d", reverse);
+
+first_digit_last_digit_sum = d + a;
+total_sum = a + b + c + d;
+
+
+printf("The reverse number is:%d\n", reverse);
+printf("The sum of first and last digit is: %d\n", first_digit_last_digit_sum);
+printf("Total sum is:%d",total_sum);
 
 
 
